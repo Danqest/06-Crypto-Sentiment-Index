a881da52179bdc1a608cbf1e885b51ff97002f0c
Colin Brooks <mcbrooks@pm.me>
2022-04-14 21:05:47 -0400

    fixed js and added html chart

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+
 // All endpoints return either a JSON object or array.
 // Data is returned in ascending order. Oldest first, newest last.
 // All time and timestamp related fields are in milliseconds.
@@ -7,14 +9,17 @@ var endPointURL2 = 'https://api2.binance.com/'
 var endPointURL3 = 'https://api3.binance.com/'
 var requestHeader = 'api/v3/'
 
+var x = []
+var opn = []
+var high = []
+var low = []
+var cls = []
+var exampleArray = []
+
 // var cryptoTicker = document.querySelector('.crypto-ticker').value
 // var timeFrame = document.querySelector('.date').value
 
-function init() {
-    getAPI()
-}
-
-// var requestUrl = 'https://api.binance.com/api/v3/klines?symbol=ETHBTC&interval=1d&startTime=1617249600000&endTime=1617854400000'
+var requestUrl = 'https://api.binance.com/api/v3/klines?symbol=ETHBTC&interval=1d&startTime=1647316800000&endTime=1649908800000'
 
 function getAPI() {
     fetch(requestUrl)
@@ -22,17 +27,77 @@ function getAPI() {
       return response.json();
     })
     .then(function (data) {
-        console.log(data);
-        // var results = data.results;
-        // results.forEach(function (item) {
-        //   var title = item.title;
-        //   var date = item.date || "No Date Provided";
-        //   var subjects = item.subject || [];
-        //   var descriptions = item.description;
-        //   var url = item.url;
-        //   console.log(title, date, subjects, descriptions, url);
+        for (let i = 0; i < data.length; i++) {
+            x.push(new Date(data[i][0]).toLocaleDateString("en-US"))
+            opn.push(parseFloat(data[i][1]))
+            high.push(parseFloat(data[i][2]))
+            low.push(parseFloat(data[i][3]))
+            cls.push(parseFloat(data[i][4]))
+        }
+        console.log(x, opn, high, low, cls)
+        d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv', function(err, rows){
+
+        function unpack(rows, key) {
+        return rows.map(function(row) {
+            return row[key];
         });
+        }
+
+        var trace = {
+        x: x,
+        close: cls,
+        high: high,
+        low: low,
+        open: opn,
+
+        // cutomise colors
+        increasing: {line: {color: 'green'}},
+        decreasing: {line: {color: 'red'}},
+
+        type: 'candlestick',
+        xaxis: 'x',
+        yaxis: 'y'
+        };
+
+        var data = [trace];
+
+        var layout = {
+        dragmode: 'zoom',
+        showlegend: false,
+        xaxis: {
+            autorange: true,
+            title: 'Date',
+            rangeselector: {
+                x: 0,
+                y: 1.2,
+                xanchor: 'left',
+                font: {size:8},
+                buttons: [{
+                    step: 'month',
+                    stepmode: 'backward',
+                    count: 1,
+                    label: '1 month'
+                }, {
+                    step: 'month',
+                    stepmode: 'backward',
+                    count: 6,
+                    label: '6 months'
+                }, {
+                    step: 'all',
+                    label: 'All dates'
+                }]
+            }
+        },
+        yaxis: {
+            autorange: true,
+        }
+        };
+
+        Plotly.newPlot('myDiv', data, layout);
+        });
+    });
 }
+
 
 
 // EXAMPLE URL TO FETCH: 
@@ -45,29 +110,48 @@ function getAPI() {
 
 // BELOW IS 4/1 TO 4/7
 
-var exampleArray = 
-[
-[1617321600000,"0.03351200","0.03630000","0.03314300","0.03619000","300198.17200000",1617407999999,"10309.15126161",361393,"149152.16700000","5124.61721012","0"],
-[1617408000000,"0.03619000","0.03626500","0.03458500","0.03520900","271274.39700000",1617494399999,"9608.31598544",319925,"138316.27700000","4899.94038079","0"],
-[1617494400000,"0.03520400","0.03596100","0.03491900","0.03566900","186558.86000000",1617580799999,"6620.49334391",229410,"93017.51700000","3301.23637700","0"],
-[1617580800000,"0.03566800","0.03610500","0.03521000","0.03563700","230293.70800000",1617667199999,"8207.89357751",312027,"110438.30500000","3936.37862681","0"],
-[1617667200000,"0.03564100","0.03660500","0.03560800","0.03641500","235222.68600000",1617753599999,"8498.17194028",326595,"113119.83200000","4086.89736999","0"],
-[1617753600000,"0.03641900","0.03659100","0.03470800","0.03509400","248961.59800000",1617839999999,"8852.94185430",360125,"118137.38200000","4202.26530036","0"],
-[1617840000000,"0.03508000","0.03593000","0.03497000","0.03581600","186761.61700000",1617926399999,"6625.87315253",233037,"89840.86000000","3187.35728312","0"]
-]
+// let exampleArray = 
+// [
+// [1617321600000,"0.03351200","0.03630000","0.03314300","0.03619000","300198.17200000",1617407999999,"10309.15126161",361393,"149152.16700000","5124.61721012","0"],
+// [1617408000000,"0.03619000","0.03626500","0.03458500","0.03520900","271274.39700000",1617494399999,"9608.31598544",319925,"138316.27700000","4899.94038079","0"],
+// [1617494400000,"0.03520400","0.03596100","0.03491900","0.03566900","186558.86000000",1617580799999,"6620.49334391",229410,"93017.51700000","3301.23637700","0"],
+// [1617580800000,"0.03566800","0.03610500","0.03521000","0.03563700","230293.70800000",1617667199999,"8207.89357751",312027,"110438.30500000","3936.37862681","0"],
+// [1617667200000,"0.03564100","0.03660500","0.03560800","0.03641500","235222.68600000",1617753599999,"8498.17194028",326595,"113119.83200000","4086.89736999","0"],
+// [1617753600000,"0.03641900","0.03659100","0.03470800","0.03509400","248961.59800000",1617839999999,"8852.94185430",360125,"118137.38200000","4202.26530036","0"],
+// [1617840000000,"0.03508000","0.03593000","0.03497000","0.03581600","186761.61700000",1617926399999,"6625.87315253",233037,"89840.86000000","3187.35728312","0"]
+// ]
 
-for (var i = 0; i < exampleArray.length; i++) {
-    // console.log(exampleArray[i].slice(0,7))
-    var date = new Date(exampleArray[i][0]).toLocaleDateString("en-US")
-    var open = exampleArray[i][1]
-    var high = exampleArray[i][2]
-    var low = exampleArray[i][3]
-    var close = exampleArray[i][4]
-    var volume = exampleArray[i][5]
-    console.log(date, open, high, low, close, volume)
+
+
+function buildArrays(exampleArray) {
+    for (let i = 0; i < exampleArray.length; i++) {
+        x.push(new Date(exampleArray[i][0]).toLocaleDateString("en-US"))
+        opn.push(parseFloat(exampleArray[i][1]))
+        high.push(parseFloat(exampleArray[i][2]))
+        low.push(parseFloat(exampleArray[i][3]))
+        cls.push(parseFloat(exampleArray[i][4]))
+    }
+    console.log(x)
+    console.log(opn)
+    console.log(high)
+    console.log(low)
+    console.log(cls)
+    console.log(exampleArray)
 }
 
-var masterDict = {}
 
-// var d = new Date((exampleArray[0][6])).toLocaleDateString("en-US")
-// console.log(d)
+// console.log(x)
+// console.log(opn)
+// console.log(high)
+// console.log(low)
+// console.log(cls)
+// console.log(exampleArray)
+
+
+
+
+
+
+exampleArray = getAPI()
+console.log(exampleArray)
+// buildArrays()
